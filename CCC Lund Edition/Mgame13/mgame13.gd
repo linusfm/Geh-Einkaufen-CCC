4d1717e9eb37b022351cc0b5919dc36f70108e47
Linus F. Müller <97831830+linusfm@users.noreply.github.com>
2024-12-12 14:39:22 +0100

    Movement Wagen und Dose
    Das Hineinlegen der Dose in den Wagen sowie das entsprechende Mitbewegen wurde umgesetzt.

--- CCC Lund Edition/Mgame13/mgame13.gd
+++ CCC Lund Edition/Mgame13/mgame13.gd
@@ -8,7 +8,10 @@ signal win(paneln)
 
 ### Internal Constants
 var wagen_speed = 20
-
+var ueberlappung_wagen_katzenfutter = false
+var falling_dose = false
+var im_wagen = false
+var start_KF = Vector2(-5, -5)
 ### Variables
 
 func set_active(b):
@@ -19,7 +22,7 @@ func set_active(b):
 		$MusicPlayer.pause()
 	
 func new_game():
-	pass
+	$Katzenfutter/Sprite2DK.position = start_KF
 
 func enable():
 	if get_node("GameOver"):
@@ -46,13 +49,41 @@ func _ready():
 	var music = preload("res://music/mgame4.mp3")
 	$MusicPlayer.start(music)
 
+
+
 func _process(delta):
 	if Input.is_action_just_pressed("newgame"):
 		enable()
-	if Input.is_action_pressed("Left") and active == true:
-		$Einkaufswagen.position.x -= wagen_speed * delta
-	if Input.is_action_pressed("Right") and active == true:
-		$Einkaufswagen.position.x += wagen_speed * delta
+	if $Einkaufswagen.position.x > -30:
+		if Input.is_action_pressed("Left") and active == true:
+			$Einkaufswagen.position.x -= wagen_speed * delta
+			if im_wagen == true:
+				$Katzenfutter/Sprite2DK.position.x -= wagen_speed * delta
+	if $Einkaufswagen.position.x < 25:
+		if Input.is_action_pressed("Right") and active == true:
+			$Einkaufswagen.position.x += wagen_speed * delta
+			if im_wagen == true:
+				$Katzenfutter/Sprite2DK.position.x += wagen_speed * delta
+	if ueberlappung_wagen_katzenfutter:
+		if Input.is_action_pressed("Down") and active == true: 
+			falling_dose = true
+	if ueberlappung_wagen_katzenfutter:
+		if Input.is_action_pressed("Up") and active == true: 
+			falling_dose = true
+	if falling_dose == true:
+		falling_dose = false
+		var ziel_pos = Vector2($Einkaufswagen/Sprite2DE.position.x - 10, $Einkaufswagen/Sprite2DE.position.y - 10)
+		if Input.is_action_pressed("Down") and active == true and im_wagen == false:
+			$Katzenfutter/Sprite2DK.position = ziel_pos
+			im_wagen = true 
+		if Input.is_action_pressed("Up") and active == true and im_wagen == true:
+			$Katzenfutter/Sprite2DK.position = start_KF
+			im_wagen = false
+			
 
-func _on_einkaufswagen_area_entered(area: Area2D):
-	pass # Replace with function body.
+func _on_katzenfutter_area_entered(area: Area2D):
+	ueberlappung_wagen_katzenfutter = true
+	print("ueberlappung_wagen_katzenfutter")
+
+func _on_katzenfutter_area_exited(area: Area2D):
+	ueberlappung_wagen_katzenfutter = false 
